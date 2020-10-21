e850491db32216060d28055eb8cc155d5d8dd086
Maciej Furman <maciek.furman.user@gmail.com>
2020-10-21 05:50:05 +0200

    MP-3 (#5) - Import angular-calendar styling and material theme. Create basic view for planner (with calendar).

--- ClientApp/src/app/planner/planner-view/planner-view.component.ts
+++ ClientApp/src/app/planner/planner-view/planner-view.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarEvent, DAYS_OF_WEEK } from 'angular-calendar';
+import { COLORS, POLISH_MONTHS } from '../../shared/helpers/calendar.helper';
+import { registerLocaleData } from '@angular/common';
+
+import localePl from '@angular/common/locales/pl';
+registerLocaleData(localePl);
 
 @Component({
   selector: 'app-planner-view',
@@ -7,10 +13,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlannerViewComponent implements OnInit {
 
+  locale = 'pl';
+  monthName = '';
+  viewDate: Date = new Date();
+  monthNames = POLISH_MONTHS;
+  events: CalendarEvent[];
+  readonly view = 'month';
+  readonly weekStartsOn = DAYS_OF_WEEK.MONDAY;
+
   constructor() {
   }
 
   ngOnInit(): void {
+    this.getCurrentMonthName();
+    this.createEvents();
+  }
+
+  viewDateChange(): void {
+    this.getCurrentMonthName();
+  }
+
+  private getCurrentMonthName(): void {
+    this.monthName = `${this.monthNames[this.viewDate.getMonth()]} ${this.viewDate.getFullYear()}`;
+  }
+
+  private createEvents(): void {
+    this.events = [
+      {
+        start: new Date(),
+        end: new Date(),
+        title: 'Przykładowe wydarzenie',
+        color: COLORS.global,
+        allDay: true
+      } as CalendarEvent,
+      {
+        start: new Date(),
+        end: new Date(),
+        title: 'Przykładowe wydarzenie 2',
+        color: COLORS.personal,
+        allDay: true
+      } as CalendarEvent
+    ];
   }
 
 }
