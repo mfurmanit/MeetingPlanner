ec1665cb49f4ccd49fdad2c6b89bf834ff3d4304
Maciej Furman <maciek.furman.user@gmail.com>
2020-10-29 19:22:06 +0100

    MP-6 (#9) - Fix saving date in UTC format. Remove unused parse method from date-adapter implementation.

--- ClientApp/src/app/shared/helpers/date.helper.ts
+++ ClientApp/src/app/shared/helpers/date.helper.ts
@@ -2,18 +2,10 @@ import { NativeDateAdapter } from '@angular/material/core';
 
 export class DateHelper extends NativeDateAdapter {
 
-  parse(value: any): Date | null {
-    if ((typeof value === 'string') && (value.indexOf('.') > -1)) {
-      const str   = value.split('.');
-      const year  = Number(str[2]);
-      const month = Number(str[1]) - 1;
-      const date  = Number(str[0]);
-
-      return new Date(year, month, date);
-    }
-    const timestamp = typeof value === 'number' ? value : Date.parse(value);
-
-    return isNaN(timestamp) ? null : new Date(timestamp);
+  createDate(year: number, month: number, date: number): Date {
+    const localDate = super.createDate(year, month, date);
+    const offset = localDate.getTimezoneOffset() * 60000;
+    return new Date(localDate.getTime() - offset); // utcDate
   }
 
   format(date: Date, displayFormat: string): string {
